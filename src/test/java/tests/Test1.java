0cf969d8323d90576236ddcaf7e6f077075566b2
MaggieShiva <101462972+MaggieShiva@users.noreply.github.com>
2022-03-13 10:05:42 +0200

    Update Test1.java

--- src/test/java/tests/Test1.java
+++ src/test/java/tests/Test1.java
@@ -4,9 +4,10 @@ public class Test1
 {
 	public static void main(String[] args)
 	{
-		String x="ufbukfhbkf#$^&&57$^784FHJAbdulkalam";
+		String x="ufbukfhbkf#$^&&57$^784FHJAbdulkalamsir";
 		String y=x.replaceAll("[^0-9]","");
 		System.out.println(y);
 	}
 
 }
+
